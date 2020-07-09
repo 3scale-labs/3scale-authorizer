65bc99fc8c9a639bf1496aecad0acad4f71be2a9
Philip Gough <philip.p.gough@gmail.com>
2020-07-09 12:48:08 +0100

    core: Add Logger interface and noop logger

---
+++ pkg/core/logger.go
@@ -0,0 +1,16 @@
+package core
+
+type Logger interface {
+        Infof(string, ...interface{})
+        Errorf(string, ...interface{})
+        Debugf(string, ...interface{})
+}
+
+// NoOpLogger logs nothing
+type NoOpLogger struct{}
+
+func (l *NoOpLogger) Infof(s string, i ...interface{}) {}
+
+func (l *NoOpLogger) Errorf(s string, i ...interface{}) {}
+
+func (l *NoOpLogger) Debugf(s string, i ...interface{}) {}
